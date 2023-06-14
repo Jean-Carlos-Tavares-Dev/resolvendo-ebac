aeea7813ec1b583cd638ae2e5f605064c9347f23
Jean Carlos Tavares <carlosjean.t@gmail.com>
2023-06-14 19:10:39 -0300

    Acrescendo a função para calcular média

--- src/problemas/resolvendo.java
+++ src/problemas/resolvendo.java
@@ -4,7 +4,16 @@ public class resolvendo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Oi mundo");
+		double nota=4.0;
+		double nota2=3.95;
+		double nota3=6.9;
+		double nota4 = 8.9;
+		double media;
+		
+		media = (nota+nota2+nota3+nota4)/4;
+		System.out.print("Sua média é:"+ media);
+		
+		
 	}
 
 }
